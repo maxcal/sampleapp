776e21eaa33823767a7ca6778ec5d522d6584850
Max Calabrese <max.calabrese@ymail.com>
2015-05-09 00:00:09 +0200

    add seeds file and books_per_category methods

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,23 @@
 class Store < ActiveRecord::Base
 
   has_many :books
+  has_many :categories, through: :books
 
+  def books_per_category
+    categories.eager_load(:books).map do |c|
+      {
+          name: c.name,
+          count: c.books.size # Using size instead of count is important since count will always query the DB
+      }
+    end
+  end
+
+  def books_per_category2
+    categories.distinct.map do |c|
+      {
+          name: c.name,
+          count: c.books.count
+      }
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+store = Store.find_or_create_by(name: 'The Corner Shop')
+
+fantasy = Category.find_or_create_by(name: 'fantasy')
+romance = Category.find_or_create_by(name: 'fantasy')
+mystery = Category.find_or_create_by(name: 'mystery')
+classics = Category.find_or_create_by(name: 'classics')
+
+store.books.create_with(categories: [fantasy, classics]).find_or_create_by(title: 'The Hobbit')
+store.books.create_with(categories: [romance, classics]).find_or_create_by(title: 'Gone With The Wind')
+store.books.create_with(categories: [mystery, classics]).find_or_create_by(title: 'A Study in Scarlet')
+store.books.create_with(categories: [mystery]).find_or_create_by(title: 'The Sound of Glass')
