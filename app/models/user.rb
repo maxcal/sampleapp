aa40acc925980d55fe5773e620b128b568d9693a
Max Calabrese <max.calabrese@ymail.com>
2015-05-18 04:38:44 +0200

    setup has_secure_password validation example

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validate :current_password_is_correct,
+           if: :validate_password?, on: :update
+
+  def current_password_is_correct
+    unless authenticate(current_password)
+      errors.add(:current_password, "is incorrect.")
+    end
+  end
+
+  def validate_password?
+    !password.blank?
+  end
+
+  attr_accessor :current_password
 end
